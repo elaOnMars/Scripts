c894aa6a4a01dcca719a68bb8ee304aea2f95cb2
Daniela Knoll <info@daniela-knoll.de>
2016-04-16 01:45:40 +0200

    Parse with lxml

--- XML_Parser.py
+++ XML_Parser.py
@@ -1,18 +1,52 @@
-#!/usr/bin/python3
-# coding: utf-8
+#!/usr/bin/env python
+## -*- coding: iso-8859-1 -*-
+# -*- coding: utf-8 -*-
 
-# https://pythonprogramming.net/parse-website-using-regular-expressions-urllib/
-import urllib.request
-import re
+from lxml import html
+import requests
 
-url = 'http://podcasts.srf.ch/wissenschaft_drs_2_mpx.xml'
+# Page http://www.srf.ch/sendungen/wissenschaftsmagazin
+page_srf = requests.get('http://podcasts.srf.ch/wissenschaft_drs_2_mpx.xml', stream=True)
+tree_srf = html.fromstring(page_srf.content)
 
-req = urllib.request.Request(url)
-resp = urllib.request.urlopen(req)
-respData = resp.read()
+# print(tree_srf.xpath('//div[@class="module-content"]/ul/li/text()'))
 
-paragraphs = re.findall(r'<description>(.*?)</description>',str(respData))
-#print(paragraphs)
 
-for eachDescr in paragraphs:
-    print("\n", eachDescr)
+for child_of_root in tree_srf:
+   print("CHILD: ", child_of_root.tag, child_of_root.attrib)
+
+
+# http://robotframework.org/robotframework/latest/libraries/XML.html
+# http://eli.thegreenplace.net/2012/03/15/processing-xml-in-python-with-elementtree
+# for elem in tree_srf.iterfind('branch/sub-branch'):
+#   print elem.tag, elem.attrib
+
+print("============================")
+# Finding interesting elements
+for elem in tree_srf.iter():
+    #print("ELEM: ", elem.tag, elem.attrib, elem.text)
+    if elem.tag == 'title':
+        print("TITLE: ", elem.text)
+    if elem.tag == 'description':
+        print('DESCRIPTION: ', elem.text)
+    if elem.tag == 'guid':
+        print("GUID (tag, attribute, text): ", elem.tag, elem.attrib, elem.text)
+        print("\n====================\n")
+
+        
+print("ooooooooooooo")
+for event, elem in tree_srf.iterparse():
+    if event == 'end':
+        if elem.tag == 'description' and elem.text == 'Wissen':
+            print('DESCRIPTION: ', elem.text)
+            count += 1
+    elem.clear() # discard the element
+    
+    
+## Only description tag
+#count = 0
+#for elem in tree_srf.iter(tag='description'):
+#   print("ELEM: ", elem.tag, elem.attrib, elem.text)
+#   if elem.text == 'Wissen':
+#       count += 1
+#print(count)
